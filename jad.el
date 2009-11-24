fa3c02f1be34b91dad73cc7f6245ec94232dba9f
awref <awref@USALA02845.nafta.local>
2009-11-24 14:23:26 -0500

    set c-mode and csharp-mode settings

--- jad.el
+++ jad.el
@@ -5,3 +5,21 @@
 (server-start)
 
 (load "jad-org.el")
+
+;;; csharp mode
+(defun my-csharp-mode-hook ()
+  (setq c-basic-offset 4))
+(add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+
+;; Don't indent substatements' '{'
+;; http://www.chemie.fu-berlin.de/chemnet/use/info/cc-mode/cc-mode_6.html#SEC17
+(defun my-c-mode-common-hook ()
+  ;; my customizations for all of c-mode, c++-mode, objc-mode, java-mode
+  (c-set-offset 'substatement-open 0)
+  ;; other customizations can go here
+  )
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
+(add-hook 'c-mode-hook
+          (lambda ()
+            (setq c-basic-offset 8)))
