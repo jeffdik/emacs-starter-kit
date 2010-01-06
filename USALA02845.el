3ce0865f8b399693e9662fbea0aff6974fedf152
awref <awref@USALA02845.nafta.local>
2010-01-06 12:07:00 -0500

    use ProggyCleanTTSZ-12 and zenburn on USALA02845

--- USALA02845.el
+++ USALA02845.el
@@ -1,3 +1,6 @@
+(set-default-font "ProggyCleanTTSZ-12")
+(color-theme-zenburn)
+
 (setq org-agenda-files '("~/org/Work.org"
 			 "~/org/refile.org"))
 (setq org-default-notes-file "~/org/refile.org")
