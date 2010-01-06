d51a35b09acbae9c4ba8654618d55885f053fe9b
awref <awref@USALA02845.nafta.local>
2010-01-06 12:08:07 -0500

    make nuweb-mode available on USALA02845

--- USALA02845.el
+++ USALA02845.el
@@ -4,3 +4,6 @@
 (setq org-agenda-files '("~/org/Work.org"
 			 "~/org/refile.org"))
 (setq org-default-notes-file "~/org/refile.org")
+
+(add-to-list 'auto-mode-alist '( "\\.w" . nuweb-mode))
+(autoload 'nuweb-mode "nuweb" "Load nuweb-mode" t)
