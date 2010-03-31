a613b40dac9fd11acaa154c5997f3cea6ac4c50f
Jeff Dik <jeffrey.dik@agfa.com>
2010-03-31 11:18:49 -0400

    turn off mumamo chunk coloring (coloring of the backgrounds of different modes)

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(mumamo-chunk-coloring 5)
  '(nxml-slash-auto-complete-flag t)
  '(org-hide-leading-stars t)
  '(org-odd-levels-only t))
