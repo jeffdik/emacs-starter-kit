6322a936d466d24eb56582a1ed66ebb28dadc3ec
Jeff Dik <jeffrey.dik@agfa.com>
2010-03-31 11:19:20 -0400

    save bookmarks after every change

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(bookmark-save-flag 1)
  '(mumamo-chunk-coloring 5)
  '(nxml-slash-auto-complete-flag t)
  '(org-hide-leading-stars t)
